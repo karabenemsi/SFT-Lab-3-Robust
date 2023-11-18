1e216b081c5baab7fa57938d3340885d403b07ac
fllu760e <florian.lubitz@mailbox.tu-dresden.de>
2023-11-18 23:04:48 +0100

    individual test case

--- fputs_test.cpp
+++ fputs_test.cpp
@@ -30,11 +30,48 @@ const char *generateCSTR(int test_id) {
 // waiting time before querying the child's exit status
 // You might want to try using a smaller value in order to get the CI results faster,
 // but there is a chance that your tests will start failing because of the timeout
-const double wait_time = 1.0;
+const double wait_time = 0.2;
 
-void test_fputs(const TestCase &str_testCase, const TestCase &file_testCase) {
+void test_fputs(const TestCase& str_testCase, const TestCase& file_testCase) {
     // execute a single test
     // use the functions in stats.h to record all tests
+    record_start_test_fputs(str_testCase, file_testCase);
+    // generate test values
+    const char* str = generateCSTR(str_testCase.id);
+    FILE* file = generateFILE(file_testCase.id);
+    // create child process
+    pid_t pid = fork();
+    // execute fputs in child process
+    if (pid == 0) {
+        const int result = fputs(str, file);
+        exit(result);
+    } else {
+        sleep(wait_time);
+        int status;
+        pid_t w = waitpid(pid, &status, WNOHANG);
+        if (w == 0) {
+            kill(pid, SIGKILL);
+            record_timedout_test_fputs();
+        } else {
+            if (WIFEXITED(status)) {
+                const int returnval = WEXITSTATUS(status);
+                record_ok_test_fputs(returnval);
+                // if (returnval == str_testCase.expected_returnvalue) {
+                //      record_ok_test_fputs(returnval);
+                // } else {
+                //     record_error_test_fputs(returnval);
+                // }
+            } else if (WIFSIGNALED(status)) {
+                const int signal = WTERMSIG(status);
+                record_crashed_test_fputs(signal);
+            } else {
+                const int signal = WSTOPSIG(status);
+                record_stopped_test_fputs(signal);
+            }
+        }
+    }
+}
+
 int main(int argc, const char** argv) {
     // execute all tests and catch exceptions
     for (int i = 0; i < testCases_CSTR_count; i++) {
